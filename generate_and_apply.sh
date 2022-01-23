488d59ec3c0a399f8430f4e752d2f90c4e8ab796
poly000 <1348292515@qq.com>
2022-01-23 19:11:34 +0800

    refactor

--- generate_and_apply.sh
+++ generate_and_apply.sh
@@ -2,3 +2,7 @@ mkdir -p temp &&
 cat src/*.txt > temp/genshin_impact.txt
 libime_pinyindict temp/genshin_impact.txt temp/genshin_impact.dict
 
+DIST=~/.local/share/fcitx5/pinyin/dictionaries
+mkdir -p $DIST
+cp temp/genshin_impact.dict $DIST
+
